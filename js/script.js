5b2dd09f5c4bd012dbfd21d257ab94b9fa56e962
simolorevg <simoneviaggi@libero.it>
2023-03-10 15:21:30 +0100

    Made the main structure of the script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert('hello world');s
+for(let i = 1; i <= 100; i++){
+    if( (i % 3 === 0) && (i % 5 === 0)){
+        console.log("Fizzbuzz " + i);
+    }else if (i % 3 === 0){
+        console.log("Fizz " + i);
+    }else if( i % 5 === 0){
+        console.log("Buzz " + i);
+    }else{
+        console.log(i);
+    }
+}
