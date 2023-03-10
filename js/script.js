36a5bb079de3f9ba58cdea1b8beffa4653755745
simolorevg <simoneviaggi@libero.it>
2023-03-10 15:35:24 +0100

    Making the style css section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+const rowElement = document.querySelector(".row");
 for(let i = 1; i <= 100; i++){
     if( (i % 3 === 0) && (i % 5 === 0)){
-        console.log("Fizzbuzz " + i);
+        
     }else if (i % 3 === 0){
-        console.log("Fizz " + i);
+        
     }else if( i % 5 === 0){
-        console.log("Buzz " + i);
+        
     }else{
-        console.log(i);
+        
     }
+    rowElement.innerHTML += `<p>${i}</p>`;
 }
