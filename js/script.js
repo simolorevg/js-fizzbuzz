e8851495e328667ae8e19a4472ff6974e5c3d4b9
simolorevg <simoneviaggi@libero.it>
2023-03-10 16:02:57 +0100

    Main part of the project terminated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 const rowElement = document.querySelector(".row");
+let classElement;
+let numIdentity;
 for(let i = 1; i <= 100; i++){
     if( (i % 3 === 0) && (i % 5 === 0)){
-        
+        classElement = "fizz-buzz";
+        numIdentity = "Fizzbuzz";
+        rowElement.innerHTML += `<div class="column ${classElement}">${numIdentity}</div>`;
     }else if (i % 3 === 0){
-        
+        classElement = "fizz";
+        numIdentity= "Fizz";
+        rowElement.innerHTML += `<div class="column ${classElement}">${numIdentity}</div>`;
     }else if( i % 5 === 0){
-        
+        classElement = "buzz";
+        numIdentity = "Buzz";
+        rowElement.innerHTML += `<div class="column ${classElement}">${numIdentity}</div>`;
     }else{
-        
+        numIdentity = i;
+        rowElement.innerHTML += `<div class="column">${numIdentity}</div>`;
     }
-    rowElement.innerHTML += `<p>${i}</p>`;
+    
+    console.log(i, numIdentity);
+    
 }
